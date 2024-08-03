bc6a8a0001ba994e4f8cc35585aac53c65a5e1e2
aitunin <2323353@gmail.com>
2024-08-04 01:16:39 +0300

    otel operator

--- commands.sh
+++ commands.sh
@@ -34,3 +34,5 @@ kubectl create secret generic db-secret --from-literal=username=postgres --from-
 minikube addons enable ingress
 minikube ip
 
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.2/cert-manager.yaml
+kubectl apply -f https://github.com/open-telemetry/opentelemetry-operator/releases/latest/download/opentelemetry-operator.yaml
